d9e89f798131ace756c5b3b6b83da1ad3c7345f8
Alejo2408 <151172730+Alejo2408@users.noreply.github.com>
2025-08-14 09:19:26 -0300

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -94,7 +94,7 @@ const Services = () => {
                 ))}
               </ul>
 
-              <motion.button 
+              {/*<motion.button 
                 className={w-full mt-8 bg-gradient-to-r ${service.color} text-white py-3 rounded-xl font-semibold hover:shadow-lg transition-all duration-300}
                 whileHover={{ scale: 1.02 }}
                 whileTap={{ scale: 0.98 }}
@@ -102,7 +102,7 @@ const Services = () => {
                 Solicitar Servicio
               </motion.button>
             </motion.div>
-          ))}
+          ))}*/}
         </div>
 
         {/* Why Choose Us */}
